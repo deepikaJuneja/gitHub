cd0489d8cf3f2211ee9ac7201b0d3b2e8f944840
deepikaJuneja <deepikajuneja93@gmail.com>
2018-07-28 14:15:44 +0530

    this is my first attempt with GitHub

--- gitHub/ViewController.swift
+++ gitHub/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("gitHub Use for commit data")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
