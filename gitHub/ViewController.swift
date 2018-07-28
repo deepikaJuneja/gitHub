d2ba3ff46f7b162f437555b9e112961b139e49ab
deepikaJuneja <deepikajuneja93@gmail.com>
2018-07-28 15:33:08 +0530

    Add reverse func

--- gitHub/ViewController.swift
+++ gitHub/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("gitHub Use for commit data")
+        print(reverse(text: "stressed"))
+        self.view.backgroundColor = UIColor.purple
         
         // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
